a92f918ebb10f538850f62b98ebb482a0bf6ee7f
praveensivasamy <dannyboyhex@gmail.com>
2017-06-16 23:12:21 +0100

    Organize imports

--- my-commons/src/main/java/com/praveen/commons/hibernate/HibernateProvider.java
+++ my-commons/src/main/java/com/praveen/commons/hibernate/HibernateProvider.java
@@ -12,7 +12,6 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
