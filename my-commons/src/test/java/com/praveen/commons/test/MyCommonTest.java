78fbf3f55eaf0c4431904bd59499376ce0149753
praveensivasamy <dannyboyhex@gmail.com>
2017-06-23 12:18:26 +0200

    Added dummy test cases

---
+++ my-commons/src/test/java/com/praveen/commons/test/MyCommonTest.java
@@ -0,0 +1,14 @@
+package com.praveen.commons.test;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class MyCommonTest extends TestCase {
+
+	
+	@Test
+	public void test() {
+		assertTrue("Sample Test", true);
+	}
+}
---
+++ my-journal/src/test/java/com/praveen/journal/test/MyJournalLoginTest.java
@@ -0,0 +1,14 @@
+package com.praveen.journal.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MyJournalLoginTest {
+
+	@Test
+	public void test() {
+		assertTrue("Sample Test", true);
+	}
+
+}
