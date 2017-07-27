a841c3991c87d4cdecbab37e9343f3561fffd412
praveensivasamy <dannyboyhex@gmail.com>
2017-07-27 12:28:52 +0200

    first commit

---
+++ mapping/src/com/mapping/parser/InvoicePostingTrackerParser.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.mapping.parser;
+
+/**
+ * @author EH2SI2V
+ *
+ */
+public class InvoicePostingTrackerParser {
+
+}
---
+++ mapping/src/com/mapping/parser/bo/PostingTracker.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.mapping.parser.bo;
+
+/**
+ * @author EH2SI2V
+ *
+ */
+public class PostingTracker {
+
+}
